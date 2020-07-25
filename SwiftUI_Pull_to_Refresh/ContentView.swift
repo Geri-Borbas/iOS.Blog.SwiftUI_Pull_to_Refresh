5a9fb97a764de21175fd7b832d5108b73e65b631
Geri Borbás <geri@eppz.eu>
2020-07-25 15:20:43 +0200

    Initial Commit

---
+++ SwiftUI_Pull_to_Refresh/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUI_Pull_to_Refresh
+//
+//  Created by Geri Borbás on 2020. 07. 25..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
