c2497cd7660234f267f1778f7b15a2cf13877274
Geri Borbás <geri@eppz.eu>
2022-03-21 13:03:32 +0100

    1.5.11

--- SwiftUI_Pull_to_Refresh/Prototypes/Sketches/FitTextView.swift
+++ SwiftUI_Pull_to_Refresh/Prototypes/Sketches/FitTextView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 
 extension View {
-
+	
 	func heroStyle(black: Bool = true) -> some View {
 		self
 			.font(Font.custom(black ? "Lato-Black" : "Lato-Regular", size: 68))
@@ -20,13 +20,13 @@ extension View {
 
 
 struct FitTextView: View {
-		
+	
 	static let screenFrame = CGRect(
 		x: 0,
 		y: 44,
 		width: 375,
 		height: 812 - 44
-	)S
+	)
 	
 	var body: some View {
 		ZStack {
@@ -34,19 +34,15 @@ struct FitTextView: View {
 				.fill(ImagePaint(image: Image("10pt")))
 				.opacity(0.2)
 			VStack {
-				VStack(spacing: 0) {
-					SummaryView(
-						imageName: "text.book.closed.fill",
-						celsius: "-65.4",
-						description: "Few Clouds"
-					)
-						.environment(\.screenFrame, FitTextView.screenFrame)
-					AttributesView(
-						wind: "0.71",
-						humidity: "85",
-						uv: "1.2"
-					)
-				}
+				WeatherView(
+					imageName: "text.book.closed.fill",
+					celsius: "-65.4",
+					description: "Few Clouds",
+					wind: "0.71",
+					humidity: "85",
+					uv: "1.2"
+				)
+					.redLine(opacity: 0.5)
 				Spacer()
 			}
 			.padding(.horizontal, UI.padding)
