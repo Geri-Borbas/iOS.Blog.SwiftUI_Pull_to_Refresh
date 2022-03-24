45787d7431461d72610a986024b7fb693f83fe92
Geri Borbás <geri@eppz.eu>
2022-03-24 18:53:34 +0100

    0.0.2

--- SwiftUI_Pull_to_Refresh/Extensions/UIScrollView+Extensions.swift
+++ SwiftUI_Pull_to_Refresh/Extensions/UIScrollView+Extensions.swift
@@ -11,90 +11,98 @@ import UIKit
 
 extension UIScrollView {
 	
-	func swizzle() {
+	static var speed = UI.Speed()
+
+	func set(speed: UI.Speed) {
+		
+		// Only if tweaked.
+		guard speed.layerSpeed != 1.0 else {
+			return
+		}
+		
+		// Save.
+		Self.speed = speed
+		
+		print("original decelerationRate: \(self.decelerationRate.rawValue)")
+		
+		// Scroll deceleration.
+		self.decelerationRate = .init(rawValue: speed.decelerationRate)
+		self.setValue(speed.pagingFriction, forKey: "pagingFriction")
+		self.setValue(speed.decelerationRate, forKey: "verticalScrollDecelerationFactor")
+		self.setValue(speed.decelerationRate, forKey: "horizontalScrollDecelerationFactor")
+		
+		// Bounce.
+		swizzleGetBouncingDecelerationOffset()
+	}
+	
+	func swizzleGetBouncingDecelerationOffset() {
 		
 		guard let scrollViewClass: AnyClass = object_getClass(self) else {
-			return print("Could not get class.")
+			return print("Could not get `UIScrollView` class.")
 		}
 		
-		let selector = Selector("_getPagingDecelerationOffset:forTimeInterval:")
+		let selectorName = "_getBouncingDecelerationOffset:forTimeInterval:lastUpdateOffset:min:max:decelerationFactor:decelerationLnFactor:velocity:"
+		let selector = Selector(selectorName)
 		guard let method = class_getInstanceMethod(scrollViewClass, selector) else {
-			return print("Could not get selector.")
+			return print("Could not get `getBouncingDecelerationOffset()` selector.")
 		}
 		
-		guard let originalMethod = class_getInstanceMethod(scrollViewClass, #selector(originalGetPagingDecelerationOffset(_:forTimeInterval:))) else {
-			return print("Could not get original selector.")
+		let originalSelector = #selector(originalGetBouncingDecelerationOffset(_:forTimeInterval:lastUpdateOffset:min:max:decelerationFactor:decelerationLnFactor:velocity:))
+		guard let originalMethod = class_getInstanceMethod(scrollViewClass, originalSelector) else {
+			return print("Could not get original `getBouncingDecelerationOffset()` selector.")
 		}
 		
-		guard let swizzledMethod = class_getInstanceMethod(scrollViewClass, #selector(swizzleGetPagingDecelerationOffset(_:forTimeInterval:))) else {
-			return print("Could not get swizzled selector.")
+		let swizzledSelector = #selector(swizzledGetBouncingDecelerationOffset(_:forTimeInterval:lastUpdateOffset:min:max:decelerationFactor:decelerationLnFactor:velocity:))
+		guard let swizzledMethod = class_getInstanceMethod(scrollViewClass, swizzledSelector) else {
+			return print("Could not get swizzled `getBouncingDecelerationOffset()` selector.")
 		}
 		
-		// Swap.
+		// Swap implementations.
 		method_exchangeImplementations(method, originalMethod)
 		method_exchangeImplementations(method, swizzledMethod)
 	}
 	
-//	- (BOOL)_getPagingDecelerationOffset:(struct CADoublePoint { double x1; double x2; }*)arg1 forTimeInterval:(double)arg2;
+//	- (BOOL)_getBouncingDecelerationOffset:(double*)arg1
+//	forTimeInterval:(double)arg2
+//	lastUpdateOffset:(double)arg3
+//	min:(double)arg4
+//	max:(double)arg5
+//	decelerationFactor:(double)arg6
+//	decelerationLnFactor:(double)arg7
+//	velocity:(double*)arg8;
 	
-	@objc func originalGetPagingDecelerationOffset(_ arg1: UnsafeMutablePointer<CGPoint>, forTimeInterval arg2: TimeInterval) -> Bool {
-		return true
+	@objc func originalGetBouncingDecelerationOffset(
+		_ bouncingDecelerationOffset: UnsafeMutablePointer<CGFloat>,
+		forTimeInterval timeInterval: TimeInterval,
+		lastUpdateOffset: TimeInterval,
+		min: CGFloat,
+		max: CGFloat,
+		decelerationFactor: CGFloat,
+		decelerationLnFactor: CGFloat,
+		velocity: UnsafeMutablePointer<CGFloat>
+	) -> Bool {
+		return true // Original implementation will be copied here
 	}
 	
-	@objc func swizzleGetPagingDecelerationOffset(_ arg1: UnsafeMutablePointer<CGPoint>, forTimeInterval arg2: TimeInterval) -> Bool {
-		
-		print("arg1: \(arg1.pointee)")
-		print("arg2: \(arg2)")
-		
-		return originalGetPagingDecelerationOffset(arg1, forTimeInterval: arg2)
-	}
-	
-		// Bouncing.
-//		- (BOOL)_getBouncingDecelerationOffset:(double*)arg1
-//		forTimeInterval:(double)arg2
-//		lastUpdateOffset:(double)arg3
-//		min:(double)arg4
-//		max:(double)arg5
-//		decelerationFactor:(double)arg6
-//		decelerationLnFactor:(double)arg7
-//		velocity:(double*)arg8;
-
-//		- (void)_getStandardDecelerationOffset:(double*)arg1
-//		forTimeInterval:(double)arg2
-//		min:(double)arg3
-//		max:(double)arg4
-//		decelerationFactor:(double)arg5
-//		decelerationLnFactor:(double)arg6
-//		velocity:(double*)arg7;
-
-
-	func set(speed: CGFloat) {
-		
-		swizzle()
-		
-		// Only on change.
-		guard speed != 1 else {
-			return
-		}
-		
-		// Scroll deceleration.
-		let decelerationRate = self.decelerationRate.rawValue
-		print("decelerationRate: \(decelerationRate)")
-		let decelerationRateExponent = pow(decelerationRate, speed)
-		set(decelerationRate: decelerationRateExponent)
-		
-		// Paging.
-		let pagingFriction: CGFloat = value(forKey: "pagingFriction") as? CGFloat ?? 0
-		print("pagingFriction: \(pagingFriction)")
-		let pagingFrictionExponent = pow(pagingFriction, 1 / speed)
-		set(pagingFriction: pagingFrictionExponent)
-	}
-	
-	fileprivate func set(decelerationRate: CGFloat) {
-		self.decelerationRate = .init(rawValue: decelerationRate)
-	}
-	
-	fileprivate func set(pagingFriction: CGFloat) {
-		self.setValue(pagingFriction, forKey: "pagingFriction")
+	@objc func swizzledGetBouncingDecelerationOffset(
+		_ bouncingDecelerationOffset: UnsafeMutablePointer<CGFloat>,
+		forTimeInterval timeInterval: TimeInterval,
+		lastUpdateOffset: TimeInterval,
+		min: CGFloat,
+		max: CGFloat,
+		decelerationFactor: CGFloat,
+		decelerationLnFactor: CGFloat,
+		velocity: UnsafeMutablePointer<CGFloat>
+	) -> Bool {
+		return originalGetBouncingDecelerationOffset(
+			bouncingDecelerationOffset,
+			forTimeInterval: timeInterval * CGFloat(Self.speed.layerSpeed),
+			lastUpdateOffset: lastUpdateOffset,
+			min: min,
+			max: max,
+			decelerationFactor: decelerationFactor,
+			decelerationLnFactor: decelerationLnFactor,
+			velocity: velocity
+		)
 	}
 }
